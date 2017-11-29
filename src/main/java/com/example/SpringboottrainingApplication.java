f2d0f930500c4df3bacbb42853b8e70f2184c923
conceptviolator <conceptviolator@gmail.com>
2017-11-29 16:58:28 +0300

    first commit

---
+++ src/main/java/com/example/SpringboottrainingApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottrainingApplication.class, args);
+	}
+}
